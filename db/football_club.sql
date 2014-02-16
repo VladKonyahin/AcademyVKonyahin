e540e8aa69b407b7005e155a3d8eba30c2a87570
VladKonyahin <vladkonyahin@gmail.com>
2014-02-16 20:37:24 +0200

    add db

---
+++ db/football_club.sql
@@ -0,0 +1,87 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.1
+-- http://www.phpmyadmin.net
+--
+-- Хост: 127.0.0.1
+-- Время создания: Фев 16 2014 г., 20:34
+-- Версия сервера: 5.5.25
+-- Версия PHP: 5.3.13
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- База данных: `football_club`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `club`
+--
+
+CREATE TABLE IF NOT EXISTS `club` (
+  `club_id` int(10) NOT NULL AUTO_INCREMENT,
+  `club_name` varchar(100) NOT NULL,
+  `city` varchar(100) NOT NULL,
+  `country` varchar(100) NOT NULL,
+  `division` varchar(100) NOT NULL,
+  `year of foundation` date NOT NULL,
+  `trophies` int(10) NOT NULL,
+  `budget` int(10) NOT NULL,
+  `president` varchar(100) NOT NULL,
+  `stadium` varchar(100) NOT NULL,
+  PRIMARY KEY (`club_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `league`
+--
+
+CREATE TABLE IF NOT EXISTS `league` (
+  `league_id` int(10) NOT NULL AUTO_INCREMENT,
+  `league_name` varchar(100) NOT NULL,
+  `league_counry` varchar(100) NOT NULL,
+  `rating` int(10) NOT NULL,
+  `league_president` varchar(100) NOT NULL,
+  PRIMARY KEY (`league_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `stadium`
+--
+
+CREATE TABLE IF NOT EXISTS `stadium` (
+  `stadium_id` int(10) NOT NULL AUTO_INCREMENT,
+  `stadium_name` varchar(100) NOT NULL,
+  `stadium_foundation` date NOT NULL,
+  `capacity` int(10) NOT NULL,
+  PRIMARY KEY (`stadium_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `trophies`
+--
+
+CREATE TABLE IF NOT EXISTS `trophies` (
+  `trophie_id` int(10) NOT NULL AUTO_INCREMENT,
+  `trophie_name` varchar(100) NOT NULL,
+  `trophie_foundation` date NOT NULL,
+  PRIMARY KEY (`trophie_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
